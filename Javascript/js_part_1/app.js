d09a506542698913eb3763503834ecd7d86c35d0
Vishal-Nishad <vishal.n1020@gmail.com>
2026-03-29 16:26:18 +0530

    learning javascript

---
+++ Javascript/js_part_1/app.js
@@ -0,0 +1,31 @@
+const student = {
+    name: "vishal",
+    age:23,
+    marks:94.5
+};
+
+let student2 = ["vishal",23,94.5];
+
+const item = {
+    price: 100.99,
+    discount:50,
+    colors:["red","pink"]
+};
+
+const post = {
+    username: "@vishal123",
+    content: "This is my @firstpost",
+    likes:150,
+    reposts:5,
+    tags:["@first","newjoined"]
+}
+
+// js convert objects literal keys into string automatically
+
+const example = {
+    1:"a",
+    2:"b",
+    null:"c",
+    true:"d",
+    undefined:"e"
+}
---
+++ Javascript/js_part_1/readme.md
@@ -0,0 +1,10 @@
+# 5. Automatic object literals keys tor string conversion in js 
+    ``` js
+    const example = {
+        1:"a",
+        2:"b",
+        null:"c",
+        true:"d",
+        undefined:"e"
+    } 
+    ```
