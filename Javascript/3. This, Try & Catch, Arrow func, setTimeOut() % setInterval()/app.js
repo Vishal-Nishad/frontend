051a624104446af40e6ebb1c090ed3b1f5818e61
Vishal-Nishad <vishal.n1020@gmail.com>
2026-04-01 23:38:24 +0530

    setTimeout in js

--- Javascript/3. This, Try & Catch, Arrow func, setTimeOut() % setInterval()/app.js
+++ Javascript/3. This, Try & Catch, Arrow func, setTimeOut() % setInterval()/app.js
@@ -33,3 +33,30 @@ try{
 //     console.log("a is not defined")
 //     console.log(err)
 // }
+
+// 3. Arrow function
+// const func_name = (arg1,....argn) =>{function defintion}
+
+const greet = () => {
+    console.log("hello vishal")
+}
+greet()
+
+const sum = (a,b) =>{
+    console.log(a+b)
+}
+sum(5,6)
+
+// 4. Arrow function implicit return
+const prod = (a,b)=>(a*b)
+console.log(prod(5,5))
+
+
+//5. setTimeout function
+// setTimeout(func- callback,timeout in ms)
+
+console.log("hi there")
+setTimeout(()=>{
+    console.log("vishal learning")
+},4000);
+console.log("finally printed")
