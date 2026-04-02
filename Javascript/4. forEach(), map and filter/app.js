d4b236feff8bd622e35487f1b18ef246723e3626
Vishal-Nishad <vishal.n1020@gmail.com>
2026-04-02 23:30:11 +0530

    forEach method definition and usage

---
+++ Javascript/4. forEach(), map and filter/app.js
@@ -0,0 +1,27 @@
+// 01. forEach 
+// arr.forEach(some function definition or name);
+ar = [1,2,3,4];
+let print = function(el){
+    console.log(el)
+}
+
+ar.forEach(print)
+
+let st = [
+    {
+    name:"vishal",
+    marks:100
+    },
+    {
+    name:"hela",
+    marks:98
+    },
+    {
+    name:"reva",
+    marks:67
+    }
+]   
+
+st.forEach((student)=>{
+    console.log(student.marks)
+})
