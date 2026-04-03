2252cba36f7cc3d7ad89b41479bd252ace3aa3b2
Vishal-Nishad <vishal.n1020@gmail.com>
2026-04-04 00:06:50 +0530

    filter function example

--- Javascript/4. forEach(), map and filter/app.js
+++ Javascript/4. forEach(), map and filter/app.js
@@ -28,10 +28,10 @@ st.forEach((student)=>{
 
 
 //2. Map and filter function
-// let variable = arr.map(some function or name)
-let num = [1,2,3,4]
-let doublt = num.map((x)=>{console.log(x*2)})
-doublt
+// let newArr = arr.map(some function or name)
+let numarr = [1,2,3,4]
+let doublt = numarr.map((x)=>{return x*2})
+console.log(doublt)
 
 let gpa = st.map((el)=>{
     return el.marks/10;
@@ -39,3 +39,8 @@ let gpa = st.map((el)=>{
 console.log(gpa)
 
 // filter function
+let num = [3,2,4,5,2,4,6,7,9,100]
+let evenNum = num.filter((x)=>{
+    return x%2==0;
+})
+console.log(evenNum)
