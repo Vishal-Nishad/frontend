aa9a0d4b5f40e7ec02b8db77b676f6aaca3ee30a
Vishal-Nishad <vishal.n1020@gmail.com>
2026-04-11 18:10:27 +0530

    double click event listener

--- Javascript/6. DOM Events, Events Listener/app.js
+++ Javascript/6. DOM Events, Events Listener/app.js
@@ -4,14 +4,14 @@
 
 let btns = document.querySelectorAll("button")
 // console.dir(btn)
-for(btn of btns){
-    btn.onclick = sayHello;   // here if write like this sayHello() then it instantly start
+for(let i= 0; i<btns.length -1;i++){
+    btns[i].onclick = sayHello;   // here if write like this sayHello() then it instantly start
     // executing whenever this page gets opened that's why we write sayHello and on 
     // onclick it gets executed
-    btn.onmouseenter = function(){
+    btns[i].onmouseenter = function(){
         console.log("you are hovering over button")
     }
-    console.dir(btn)
+    console.dir(btns[i])
 }
 
 // btn.onclick = function(){
@@ -22,9 +22,26 @@ function sayHello(){
     alert("hello!");
 }
 
-btn.onclick = sayHello;
+function sayName(){
+    alert("how are you vishal")
+}
 
+// btn.onclick = sayHello;
+
+// onclick only works for one time click means we can do multiple things on that single click
+// that's why we use mostly event listeners for mutliple work on single click on any 
+// button
 
 
 // 👉 3. Event Listeners
 // addEventListener
+// element.addEventListener(even,callback)
+
+let btn2 = document.querySelector("#addEventListener")
+btn2.addEventListener("click",sayHello)
+btn2.addEventListener("click",sayName)
+
+let btn3 = document.querySelector("#addEventListenerDbc")
+btn3.addEventListener("dblclick", function (){
+    console.log("you double clicked me");
+})
