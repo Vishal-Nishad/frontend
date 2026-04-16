bd6a86393daa757d5c1a412f471f9650fa9b9fe0
Vishal-Nishad <vishal.n1020@gmail.com>
2026-04-16 22:10:07 +0530

    why promises exist when to use

--- Javascript/8. CallStack, JS is Single Threaded, Callback Hell/app.js
+++ Javascript/8. CallStack, JS is Single Threaded, Callback Hell/app.js
@@ -28,13 +28,13 @@ function three(){
 
 
 // 👉 4. JS in Single Threaded
-setTimeout(()=>{
-    console.log("apna college");
-},2000);
-setTimeout(()=>{
-    console.log("i am vishal");
-},2000);
-console.log("hello")
+// setTimeout(()=>{
+//     console.log("apna college");
+// },2000);
+// setTimeout(()=>{
+//     console.log("i am vishal");
+// },2000);
+// console.log("hello")
 
 
 
@@ -70,3 +70,85 @@ changeColor("red",1000,()=>{
         })
     })
 })
+
+
+
+// 👉 6. Setting up for Promises
+// below is also a real prod like example of callback hell, which promises solved easily
+function saveToDb(data,success,failure){
+    let internetSpeed = Math.floor(Math.random() * 10 + 1);
+    if(internetSpeed > 4){
+        success();
+    }else{
+        failure();
+    }
+}
+
+saveToDb("vishal",()=>{
+    console.log("success: your data was saved");
+    saveToDb("hello vishal", ()=>{
+        console.log("success2: data2 saved");
+        saveToDb("how are you",()=>{
+            console.log("success3: data3 saved")
+        },
+        ()=>{
+            console.log("failure3: weak connection")
+        }    )
+    }, ()=>{
+        console.log("failure2: weak connection")
+    })
+},
+()=>{
+    console.log("failure: weak connection, data not saved");
+}
+
+)
+
+
+// 👉 7. Refactoring above callback hell with promises
+// Promises -> the promise object represents the eventual completion(or failure) of an
+// asynchronous operation and its resulting value.
+
+// Promises in JS is object having two things -> resolve and reject
+
+function savedToDb(data){
+    return new Promise((resolve,reject)=>{ // whenever a promise called these two args are by default present
+        let internetSpeed = Math.floor(Math.random()*10 + 1);
+        if(internetSpeed > 4){
+            resolve("success: data was saved");
+        }else{
+            reject("failure: weak connection");
+        }
+    });
+}
+console.log("\n")
+
+// 👉 8. as Promises is object so it has two methods .then() and .catch 
+// .then for resolve/success and .catch for reject/failure
+
+let request = savedToDb("hi vishal") // now request is that promise object which savedToDb returned
+
+request.then(()=>{
+    console.log("promise was resolved");
+    console.log(request)
+}).catch(()=>{
+    console.log("promise was rejected")
+    console.log(request)
+})
+
+
+// more short/compact way to write directly on async/await function means which returns promises
+savedToDb("vishal").then(()=>{
+    console.log("   \n") 
+    console.log("\n")
+    console.log("direct: promise was resolved");
+    console.log(request)
+}).catch(()=>{
+    console.log("direct: promise was rejected")
+    console.log(request)
+})
+
+
+
+
+// 👉 9. Promise chaining
