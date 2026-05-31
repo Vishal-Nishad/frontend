ef89fa9f0a3edd40f8330249fbec2eb6cd1ae3d1
Vishal-Nishad <vishal.n1020@gmail.com>
2026-06-01 00:21:38 +0530

    State in React

--- React/basic-react-app/src/part_3/App.jsx
+++ React/basic-react-app/src/part_3/App.jsx
@@ -2,10 +2,21 @@
 // Handling click events
 import Button from './Button.jsx'
 
+// 👉 3. Events in React
+import Form from './Form.jsx';
+
+// 👉👉 4. State in React: The state is a built-in React object that is used to 
+// contain data or information about the component. A component's state can change 
+// over time; whenever it changes , the component re-renders.
+
+// 👉👉 5. Hooks: Hooks were a new addition in React 16.8. They let you use
+// state and other React features without writing a class
+
 function App(){
     return (
         <>
             <Button/>
+            <Form/>
         </>
     );
 }
