a2c03d59fadb70000794ff90e6f6af4cb4e95679
Vishal-Nishad <vishal.n1020@gmail.com>
2026-06-01 08:19:16 +0530

    useState, how it work, closure

--- React/basic-react-app/src/part_3/App.jsx
+++ React/basic-react-app/src/part_3/App.jsx
@@ -12,11 +12,42 @@ import Form from './Form.jsx';
 // 👉👉 5. Hooks: Hooks were a new addition in React 16.8. They let you use
 // state and other React features without writing a class
 
+
+// 👉👉 6. useState(): useState is a React Hook that lets you add a state variable
+// to your component.
+// const [state, setState] = useState(initialState);
+// useState returns an array with exactly two values:
+// 1. the current state. During the first render, it will match the initialState
+// you have passed.
+// 2. The set function that lets you update the state to a different value and 
+// trigger a re-render
+
+import Counter from './Counter.jsx';
+
+// 👉👉 8. Closure in JS: A closure is a feature in JS where an inner function
+// has access to the outer(enclosing) function's variables. * as in JS, once a
+// function completes its execution, any variables that were defined inside the
+// function scope gets deleted.
+// so in closure we are solving this above said problem
+/* example of closure: 
+function outer(){
+    let b = 10;
+    function inner(){
+        let a = 20;
+        console.log(a+b);
+    }
+    return inner;
+    }
+let inner = outer();
+inner()  // this will work perfectly fine, and give o/p = 30
+*/
+
 function App(){
     return (
         <>
             <Button/>
             <Form/>
+            <Counter/>
         </>
     );
 }
--- React/basic-react-app/src/part_3/Counter.jsx
+++ React/basic-react-app/src/part_3/Counter.jsx
@@ -1,13 +1,17 @@
+import {useState} from 'react'
+
 export default function Counter(){
-    let count = 0
-    function inCount(){
-        count += 1;
+    let [count, setCount] = useState(0);
+
+    // let count = 0
+    function incCount(){
+        setCount(count + 1)
         console.log(count);
     }
     return (
         <div>
             <h3>Count = {count}</h3>
-            <button onClick={inCount}>Increase Count</button>
+            <button onClick={incCount}>Increase Count</button>
         </div>
     )
 }
