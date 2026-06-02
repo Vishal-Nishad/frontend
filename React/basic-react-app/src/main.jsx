c1fdee7da37ddef3de3e095f84cd2e1f81341cb2
Vishal-Nishad <vishal.n1020@gmail.com>
2026-06-02 06:48:04 +0530

    useEffect usage

--- React/basic-react-app/src/main.jsx
+++ React/basic-react-app/src/main.jsx
@@ -2,7 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 // import App from './App.jsx'
-import App from './part_3/App.jsx'
+// import App from './part_3/App.jsx'
+// import App from './part_4/App.jsx'
+import App from './part_5/App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
